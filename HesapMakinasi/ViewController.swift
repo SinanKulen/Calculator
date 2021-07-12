3dd24161a0f7893e99e8dcc9a29c67c53e3bd453
SinanKulen <sinan.kulen@gmail.com>
2021-07-13 01:42:42 +0300

    Calculator

--- HesapMakinasi/ViewController.swift
+++ HesapMakinasi/ViewController.swift
@@ -8,12 +8,68 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var sonucLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func carpButton(_ sender: Any) {
+        if let  textFieldInt1 = Int(textField1.text!) {
+            if let  textFieldInt2 = Int(textField2.text!){
+                let sonucInt = textFieldInt1 * textFieldInt2
+                sonucLabel.text = String(sonucInt)
+            }
+        }
+        
+        
+        
+        
+        
+    }
+    
+    @IBAction func divideButton(_ sender: Any) {
+    
+        if let  textFieldInt1 = Int(textField1.text!) {
+            if let  textFieldInt2 = Int(textField2.text!){
+                let sonucInt = textFieldInt1 / textFieldInt2
+                sonucLabel.text = String(sonucInt)
+            }
+        }
+    }
+    
+    
+    @IBAction func sumButton(_ sender: Any) {
+        if let  textFieldInt1 = Int(textField1.text!) {
+            if let  textFieldInt2 = Int(textField2.text!){
+                let sonucInt = textFieldInt1 + textFieldInt2
+                sonucLabel.text = String(sonucInt)
+            }
+        }
+    }
+    
+    
+    @IBAction func minerButton(_ sender: Any) {
+        if let  textFieldInt1 = Int(textField1.text!) {
+            if let  textFieldInt2 = Int(textField2.text!){
+                let sonucInt = textFieldInt1 - textFieldInt2
+                sonucLabel.text = String(sonucInt)
+            }
+        }
+    }
+    
+    
+            
+        }
+    
+        
 
-}
+    
+
+
+    
 
